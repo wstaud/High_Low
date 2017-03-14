07c45a18a786f8007e340922d0576492ebe208da
xconstaud <xconstaud@gmail.com>
2017-03-13 22:16:27 -0500

    Add game final functionality

--- highlow.php
+++ highlow.php
@@ -1,5 +1,22 @@
 <?php
+//Create a random number and set user correct to false to repeat program
+$randomNumber = rand(1, 100);
+$userCorrect = false;
 
-$answer = rand(1, 100);
+//asks user for guess, compares against the random number. Gives user hint based on
+//if the number is higher or lower than the random number. Loop ends once user is correct and userCorrect is set to true
+do {
+    fwrite(STDOUT, 'Guess? ');
+    $answer = fgets(STDIN);
+    if ((int)$answer == (int)$randomNumber) {
+        echo "You gussed it right!" . PHP_EOL;
+        $userCorrect = true;
+    } else {
+        if ($answer < $randomNumber) {
+            echo "HIGHER!" . PHP_EOL;
+        } else {
+            echo "LOWER!" . PHP_EOL;
+        }
+    }
+} while ($userCorrect === false);
 
-echo $answer;
